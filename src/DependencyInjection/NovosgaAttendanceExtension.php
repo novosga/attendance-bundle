d19203723af4fcf5b6e1fe77af7f430839eff772
Rogerio Lino <rogeriolino@gmail.com>
2024-10-21 15:08:54 -0300

    fix: DI Extension

--- src/DependencyInjection/NovosgaAttendanceExtension.php
+++ src/DependencyInjection/NovosgaAttendanceExtension.php
@@ -15,7 +15,7 @@ namespace Novosga\AttendanceBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
